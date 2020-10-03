5609f27e578984fb8278a6bd70b16651d61acb6d
Adailson Aguiar <adailsoacj@live.com>
2020-10-03 14:52:18 -0300

    updatae transition

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
 
 import Dash from './pages/Dash/Dash';
 import Transacoes from './pages/Transacoes';
@@ -24,6 +24,7 @@ export default () => {
       <Stack.Navigator
         screenOptions={{
           headerShown: false,
+          ...TransitionPresets.SlideFromRightIOS,
         }}>
         <Stack.Screen name={pages.dash} component={Dash} />
         <Stack.Screen name={pages.contas} component={Contas} />
