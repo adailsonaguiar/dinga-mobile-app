d1fdbe38dc268118ee8afb1da244996d17dcf241
Adailson Aguiar <adailsoacj@live.com>
2020-10-12 13:48:17 -0300

    update react-native

--- android/app/src/main/java/com/dingaapp1_1/MainActivity.java
+++ android/app/src/main/java/com/dingaapp1_1/MainActivity.java
@@ -1,4 +1,4 @@
-package com.dinga;
+package com.dingaapp1_1;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "dinga";
+    return "dingaapp1_1";
   }
 }
