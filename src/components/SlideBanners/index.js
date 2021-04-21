3559c3aabbbb66893969fb8da894bc84c8ecc534
adailsonaguiar <adailsonacj@live.com>
2021-04-21 10:01:03 -0300

    estilos dos cards da home

--- src/components/SlideBanners/index.js
+++ src/components/SlideBanners/index.js
@@ -1,46 +1,27 @@
 import React from 'react';
-import {StyleSheet, Text, TouchableOpacity} from 'react-native';
-import Carousel from 'react-native-anchor-carousel';
-import {screenWidth} from '../../styles/dimensons';
 import * as S from './styles';
 
 const SlideBanners = () => {
-  const data = ['1', '2', '3', '4'];
+  const data = [
+    {
+      titleHead: 'Últimas Transações',
+      transactions: [],
+    },
+    '2',
+  ];
   const renderItem = ({item, index}) => {
     return (
-      <TouchableOpacity style={styles.item}>
-        <Text>{item}</Text>
-      </TouchableOpacity>
+      <S.CarouselItem>
+        <S.TitleCard>{item.titleHead}</S.TitleCard>
+      </S.CarouselItem>
     );
   };
 
   return (
     <S.Container>
-      <Carousel
-        style={styles.carousel}
-        data={data}
-        renderItem={renderItem}
-        itemWidth={screenWidth}
-        separatorWidth={10}
-        inActiveScale={1}
-        itemContainerStyle={{padding: 0}}
-      />
+      <S.RnAnchorCarousel data={data} renderItem={renderItem} />
     </S.Container>
   );
 };
 
 export default SlideBanners;
-
-const styles = StyleSheet.create({
-  carousel: {
-    flex: 4,
-    backgroundColor: 'yellow',
-  },
-  item: {
-    // width: '100%',
-    height: '100%',
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
